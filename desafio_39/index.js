d2ec6a90859970ccdc4967225df7bcb33279d6f4
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-02-04 18:20:23 -0300

    atualizado

--- desafio_39/index.js
+++ desafio_39/index.js
@@ -1,4 +1,5 @@
 const obj = window.document.getElementById('zero');
+var bt = window.document.getElementById('one');
 
 var vel = 5;
 var initial_width = Math.floor(Math.random()*100);
@@ -6,17 +7,43 @@ var initial_height = Math.floor(Math.random()*100);
 var controle;
 var pt = 1, pl = 1;
 var px, py;
+
+var auto;
 window.addEventListener("load", function () {
-    px = Math.floor(Math.random()*100);
-    py = Math.floor(Math.random()*100);
+    px = Math.floor(Math.random()*200);
+    py = Math.floor(Math.random()*200);
     obj.style.top = `${py}px`;
     obj.style.left = `${px}px`;
     console.log(`add px = ${typeof px}`);
-    obj.addEventListener("click", stop);
-    play()
+    auto = 1;
 });
-
-function play () {
+async function charge () {
+    obj.style.background="aqua";
+    bt.setAttribute("onclick", "stop()");
+    bt.innerText = "parar";
+}
+async function play () {
+    if(auto === 1) {
+        charge();
+        auto = 0;
+        console.log('auto = ' + auto);
+    }
+    let num = Math.floor(Math.random()*10);
+    console.log(num);
+    if (num <= 5 && py < 0){
+        pt = 1;
+        pl = 0;
+    }else if(num <= 5 && py > 600){
+        pt = -1;
+        pl = 0;
+    }else if(px < 0){
+        pl = 1;
+        pt = 0;
+    }else if(px > 800){
+        pl = -1;
+        pt = 0;
+    }
+/*
     if(px < 0){
         pl = 1;
     }else if(px > 800){
@@ -25,16 +52,22 @@ function play () {
         pt = 1;
     }else if(py > 600){
         pt = -1;
-    }
+    }*/
     console.log(`px = ${px} // py = ${py}`);
     px += pl*vel;
     py += pt*vel;
     obj.style.top = `${py}px`;
     obj.style.left = `${py}px`;
    controle = requestAnimationFrame(play); 
+   obj.setAttribute("onclick", "stop()");
 }
 
 function stop () {
+    obj.style.background="red";
     cancelAnimationFrame(controle);
-    obj.addEventListener("click", play);
+    obj.setAttribute("onclick", "play()");
+    bt.setAttribute("onclick", "play()");
+    bt.innerText = "iniciar";
+    auto = 1;
+
 }
