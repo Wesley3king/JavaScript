bbe8dd5877c93cc56a27859695514f1529ac8a81
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-02-09 16:15:35 -0300

    match exercise

---
+++ aula_46/match.js
@@ -0,0 +1,8 @@
+var strings1 = "king of shadows";
+var strings2 = "a lua que não se vê é a mais mortifera";
+var strings3 = "hora do show!";
+var tst = 'king';
+var ind = strings3.indexOf("show");
+var fin = strings3.match(/show/ig);
+
+document.write(fin + '//' + ind);
