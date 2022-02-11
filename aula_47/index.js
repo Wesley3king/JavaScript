e21c0a6c4bf4ef63a886b9f0fd6ba4235eef78b4
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-02-11 13:02:35 -0300

    strings pt 3

--- aula_47/index.js
+++ aula_47/index.js
@@ -16,4 +16,17 @@ document.write(`charCodeAt = ${txt.charCodeAt(2)} <br>`);
 document.write(`fromCharCode = ${String.fromCharCode(67)} === C <br>`);
 
 //concat
-document.write(`concat = ${txt.concat(' - going to pt 3')}`);
+document.write(`concat = ${txt.concat(' - going to pt 3')}  <br>`);
+
+//lastIndexOf
+document.write(`lastIndexOf = ${txt.lastIndexOf('pt')} <br>`);
+
+//localeCompare
+document.write(`localeCompare = ${txt.localeCompare("strings pt 2")} <br>`);
+
+//slice
+document.write(`slice = ${txt.slice(0,8)} <br>`);
+
+//split
+document.write(`splite === ${txt.split(' ')} <br>`)
+//cada virgúla signica um corte, podes acessar cada palavra contada por um array = console.log(res[1]); === pt;
