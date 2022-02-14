abb126901498364eea3ffcb903dbeb8e9cd634eb
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-02-14 13:03:21 -0300

    inicio do jogo da forca

---
+++ desafo_da_forca/index.js
@@ -0,0 +1,3 @@
+const local = window.document.querySelector('#lll');
+const palavras = [['animal','objeto','um dos quatro elementos','arvore','pode matar','tem penas','tem a noite','planeta'],['urso','pente','vento','araucaria','veneno','galinha','lua','saturno']];
+
