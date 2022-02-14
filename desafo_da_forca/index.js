45091e145dc752264b12ea5b15f09aba8dda7ba6
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-02-14 15:43:00 -0300

    forca quase funcionando

--- desafo_da_forca/index.js
+++ desafo_da_forca/index.js
@@ -1,3 +1,28 @@
 const local = window.document.querySelector('#lll');
+const local_dica = window.document.querySelector('#dica');
 const palavras = [['animal','objeto','um dos quatro elementos','arvore','pode matar','tem penas','tem a noite','planeta'],['urso','pente','vento','araucaria','veneno','galinha','lua','saturno']];
+const partes =[window.document.querySelector('#braco_left'),window.document.querySelector('#braco_right'),window.document.querySelector('#perna_right'),window.document.querySelector('#perna_left'),window.document.querySelector('#corpo2'),window.document.querySelector('#cara')];
+var espaços = [];
+var num;
 
+function verificar () {
+    let vv = window.document.querySelector('#valor').value
+    let fin = palavras[1][num].indexOf(vv);
+    if(fin !== -1) {
+        espaços[fin].innerText = palavras[1][num].match(vv);
+    }
+
+}
+
+function start () {
+    let numero = Math.round(Math.random()*(palavras[0].length-2));
+    num = numero;
+    local_dica.innerText = `dica: ${palavras[0][numero]}`;
+    console.log(palavras[1].length + '// '+ num);
+    for (let i = 0; i < palavras[1][numero].length; ++i) {
+        espaços.push(window.document.createElement('div'));
+        espaços[i].setAttribute('class', 'vazio');
+        window.document.querySelector('#letters').appendChild(espaços[i]);
+    }
+}
+start();
