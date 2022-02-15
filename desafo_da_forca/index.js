c78c4a4fa735de09b567a746b7dea9623637a0ea
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-02-15 13:57:28 -0300

    funcionando

--- desafo_da_forca/index.js
+++ desafo_da_forca/index.js
@@ -4,21 +4,37 @@ const palavras = [['animal','objeto','um dos quatro elementos','arvore','pode ma
 const partes =[window.document.querySelector('#braco_left'),window.document.querySelector('#braco_right'),window.document.querySelector('#perna_right'),window.document.querySelector('#perna_left'),window.document.querySelector('#corpo2'),window.document.querySelector('#cara')];
 var espaços = [];
 var num;
+var win_points = 0;
 
 function verificar () {
     let vv = window.document.querySelector('#valor').value
     let fin = palavras[1][num].indexOf(vv);
     if(fin !== -1) {
-        espaços[fin].innerText = palavras[1][num].match(vv);
-    }
+        let moded = palavras[1][num];
+        console.log('foi / '+moded);
 
+        for (let i  = 0;i < palavras[1][num].length;++i) {
+            let nd = moded.indexOf(vv);
+            console.log(moded +'//'+moded.indexOf(vv));
+            if (nd !== -1) {
+            espaços[nd].innerHTML = vv;
+            moded = moded.replace(`${vv}`, '*');
+            ++win_points
+            }
+        }
+        if (win_points === palavras[1][num].length) {
+            document.write('você ganhou');
+        }
+    }else{
+        
+    }
 }
 
 function start () {
     let numero = Math.round(Math.random()*(palavras[0].length-2));
     num = numero;
     local_dica.innerText = `dica: ${palavras[0][numero]}`;
-    console.log(palavras[1].length + '// '+ num);
+    //console.log(palavras[1].length + '// '+ num);
     for (let i = 0; i < palavras[1][numero].length; ++i) {
         espaços.push(window.document.createElement('div'));
         espaços[i].setAttribute('class', 'vazio');
