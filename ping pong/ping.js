5554e18973add4ca3a1ba8a047847156d51d78e2
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-02-19 17:31:17 -0300

    Update ping.js

--- ping pong/ping.js
+++ ping pong/ping.js
@@ -10,12 +10,18 @@ game = false;
 //posição
 var bolax, bolay;
 var posplayerx,posplayery,poscpux,poscpuy;
+var bolaIX, bolaIY;
 //direção
 var dirJy;
 
 //posições iniciais
 var posinitialy  =180,posinitialcpuy = 180,posinitialbolax = 475, posinitialbolay = 240;
-
+            var posjoginiY = 180;
+            var posjoginiX = 0;
+            var poscpuiniX = 930; 
+            var poscpuiniY = 180;
+            var posbolainiY = 240;
+            var posbolainiX = 475;
 //tamanhos
 var campoX = 0, campoY = 0, campoW = 960, campoH = 500;
 var barraW = 20, barraH = 140, bolaW = 20, bolaH = 20;
@@ -51,10 +57,18 @@ function primeiro_passo () {
         game  = true
         cancelAnimationFrame(frame);
         dirJy = 0;
-        bolax = posinitialbolax;
-        bolay = posinitialbolay;
-        posplayery = 0;
+        dirbolay = 0;
+        if ((Math.random()*10)< 5) {
+            dirbolax = -1;
+        }else{
+            dirbolax = 1;
+        }
+        bolax = posbolainiX;
+        bolay = posbolainiY;
+        posplayery = posjoginiY;
+        posplayerx = posjoginiX
         poscpuy = posinitialcpuy;
+        poscpux = poscpuiniX;
         controle_animation();
     }
 }
@@ -63,16 +77,39 @@ function primeiro_passo () {
 
 function controle_animation () {
     if (game) {
-        control_game();
+        control_player();
+        controle_bola();
     }
     frame = requestAnimationFrame(controle_animation);
 }
 
-function control_game () {
+function control_player () {
+    
     if (game) {
+        if ((posplayery+barraH) >= campoH || posplayery <= campoH ){
+            posplayery+= (velplayer * dirJy) * (-1);
+        }
         posplayery += velplayer * dirJy;
+        //console.log(`${posplayery} // ${soma}`);
         player.style.top= `${posplayery}px`;
     }
+}
+//controle da bola
+function controle_bola () {
+    bolax += velbola*dirbolax;
+    bolay += velbola*dirbolay;
+    //limites/colisão player
+    if ((bolax <= (posplayerx+barraW)) && (((bolay + bolaH) >= posplayery) && (bolay <= posplayery+barraH))){
+        dirbolay = ((bolay +(bolaH/2)) - (posplayery + (barraH/2)))/16;
+        dirbolax*=-1;
+    }
+    //colisão com cpu
+    if ((bolax >= poscpux-barraW) && (((bolay + bolaH) >= poscpuy) && (bolay <= poscpuy+barraH))){
+        dirbolay = ((bolay +(bolaH/2)) - (poscpuy + (barraH/2)))/16;
+        dirbolax*=-1;
+    }
+    bola.style.top = `${bolay}px`;
+    bola.style.left = `${bolax}px`;
 }
 //controles keydown/keyup
 function down () {
@@ -84,6 +121,7 @@ function down () {
         //para baixo
         dirJy = 1;
     }
+    console.log('down!!');
 }
 
 function up () {
