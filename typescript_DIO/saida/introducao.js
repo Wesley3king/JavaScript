fb569e2e9fa07b58e9b5ac2cb7b94c33b9390040
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-03-23 14:28:58 -0300

    typescript on

---
+++ typescript_DIO/saida/introducao.js
@@ -0,0 +1,18 @@
+//function somar
+function soma(a, b) {
+    return a + b;
+}
+console.log(soma(5, 6));
+var manga = {
+    nome: "pé de manga",
+    tipo: "fruta",
+    tamanho: "grande"
+};
+var jaboticaba = {
+    nome: "jaboticabeira",
+    tipo: "fruta",
+    tamanho: "pequena",
+    fruto: "manga"
+};
+console.log(jaboticaba);
+//# sourceMappingURL=introducao.js.map
