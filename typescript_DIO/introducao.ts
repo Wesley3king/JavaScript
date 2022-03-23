22d566693572590122f4ba16529ef0fa7a29186c
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-03-23 13:19:18 -0300

    desisto

--- typescript_DIO/introducao.ts
+++ typescript_DIO/introducao.ts
@@ -4,3 +4,29 @@ function soma (a: number,b: number) {
 }
 console.log(soma(5,6));
 
+//interfaces 
+
+interface trees {
+    nome : "pé de manga"|"jaboticabeira"|"pé de acerola";
+    tipo: "fruta";
+    tamanho: "pequena"|"média"|"grande";
+}
+
+const manga: trees = {
+    nome: "pé de manga",
+    tipo: "fruta",
+    tamanho: "grande"
+}
+
+interface frutos extends trees {
+    fruto: "manga"|"jaboticaba"|"acerola"
+}
+
+const jaboticaba: frutos = {
+    nome: "jaboticabeira",
+    tipo: "fruta",
+    tamanho: "pequena",
+    fruto: "manga"
+}
+
+console.log(jaboticaba);
