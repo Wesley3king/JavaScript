4a4d0ee39f6d9cade608587809a88940833901c7
Wesley3king <97459154+Wesley3king@users.noreply.github.com>
2022-04-08 15:55:07 -0300

    nave se movendo

--- nave_game/main.js
+++ nave_game/main.js
@@ -1,23 +1,71 @@
-const player = document.querySelector("#player"), vitória = document.querySelector("#win"), placar = document.querySelector("#placar");
+const player = document.querySelector("#player"), vitória = document.querySelector("#win"), placar = document.querySelector("#faltam"), frase = document.querySelector('#result'),barra = document.querySelector(".barra");
+//variaveis padrão
+var width = window.innerWidth, height = window.innerHeight,barraW = 100, barraH = 15;
+
 //variaveis do jogador
-var timedirX, timedirY, playerX, playerY;
+var timedirX = 0, timedirY = 0, playerX, playerY;
 
+//controles
+var jogo = false, animation, velocidade, pontos = 150;
 
-function tdown (tecla) {
+function  iniciar () {
+    document.addEventListener("keydown",Tdown);
+    document.addEventListener("keyup",Tup);
+    vitória.setAttribute("onclick","start()");
+    frase.textContent = "start";
+    velocidade = 3;
+    playerX = (width/2)-30;
+    playerY = 100;
+    
+}
+function start () {
+    jogo = true;
+    vitória.style.display= "none";
+
+    controle_animation();
+}
+
+function controle_animation () {
+    if (jogo) {
+        placar.innerHTML = pontos;
+        movimento();
+    }
+    animation = requestAnimationFrame(controle_animation);
+}
+
+function movimento () {
+   
+    console.log(`direcao x : ${playerX} / y : ${playerY}`);
+    console.log(`tecla dir x : ${timedirX} / y : ${timedirY}`);
+    //limites da arena left / top
+    if ((playerX < 0) || (playerX > (width-30))) {
+        timedirX *=-1.3; 
+    }
+    if ((playerY < 0) || (playerY > (height-30))) {
+        timedirY *=-1.3; 
+    }
+    playerX += (timedirX*velocidade);
+    playerY += (timedirY*velocidade);
+    player.style.left = `${playerX}px`;
+    player.style.top = `${playerY}px`;
+}
+
+iniciar();
+function Tdown (tecla) {
     let valor = tecla.keyCode;
     if (valor === 37) {//esquerda
-        timedirX = 1;
-    }else if (valor === 39) {//direita
         timedirX = -1;
+    }else if (valor === 39) {//direita
+        timedirX = 1;
     }
 
     if (valor === 38) {//cima
-        timedirY = 1;
-    }else if(valor === 40){//baixo
         timedirY = -1;
+    }else if(valor === 40){//baixo
+        timedirY = 1;
     }
 
-    if (valor === 35) {//espaço
+    if (valor === 32) {//espaço
 
     }
 }
@@ -30,7 +78,7 @@ function Tup (tecla) {
     if ((valor === 38) || (valor === 40)) {
         timedirY = 0;
     }
-    if (valor === 35) {
+    if (valor === 32) {
         
     }
 }
